2d1831fb709962ed248634dbd10ac9a599c694c4
dekoms <dekoms@naver.com>
2024-02-04 06:55:56 +0900

    fix: Router basename 수정

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 
 function App() {
   return (
     <>
-      <Router>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/movie/:id" element={<Detail />}></Route>
         </Routes>
-      </Router>
+      </BrowserRouter>
     </>
   );
 }
